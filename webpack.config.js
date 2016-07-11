560f01cf6f9cc2e781370a684fa67f71aae3205a
Danny Blue <dannybluedesign@gmail.com>
2016-07-11 16:00:50 -0400

    update build

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,13 @@ var env = process.env.NODE_ENV;
 var output = {
   libraryTarget: 'var',
   library: 'RxWorker',
-  filename: 'bundles/bundle.js'
+  filename: 'build/bundles/bundle.js'
 };
 
 var plugins = [];
 
 if (env === 'prod' || env === 'production') {
-  output.filename = 'bundles/bundle.min.js'
+  output.filename = 'build/bundles/bundle.min.js'
 
   plugins.push(new webpack.optimize.UglifyJsPlugin({
     sourceMap: false,
